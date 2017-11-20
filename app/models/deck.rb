a20b6c3ba62fe2a14515717feee9b7856e57df3d
Karlie & Toby Ladislas & Nicole Gasperini <pair+karlieromas.majesty86.nicole7@devbootcamp.com>
2017-11-20 15:54:18 -0600

    Add user links based on login status.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,16 @@
 class Deck < ApplicationRecord
   has_many :cards
+
+  def shuffle_cards
+    self.cards.shuffle
+  end
+
 end
+
+# Shuffle method for deck
+# get_next_card method
+# if correct guess, can't get that card anymore
+# If all cards answered correctly, go to success page of some sort
+  # later, refactor to rounds and stats.
+
+
