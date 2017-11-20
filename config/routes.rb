da015308d74649b1c5cd1d5518e816e93cdf0b8c
Karlie & Toby Ladislas & Nicole Gasperini <pair+karlieromas.majesty86.nicole7@devbootcamp.com>
2017-11-20 15:37:21 -0600

    Finally fix login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
     resources :cards
   end
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  # get '/signup', to: 'users#new'
+  # post '/users', to: 'users#create'
+
+  resources :users, only: [:new, :create]
+
 end
